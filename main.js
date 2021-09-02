c8c7948a44f70ebe088682fef253016e28d3d410
nawrin120e <nawrinsultana120@gmaile.com>
2021-09-02 20:58:20 +0600

    final commit

--- main.js
+++ main.js
@@ -1,45 +1,62 @@
+const toggleResult = (id, display) => {
+    document.getElementById(id).style.display = display;
+}
 const searchBooksInfo = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = '';
-    // console.log(searchText)
-
+    toggleResult('spinnner', 'block')
+    toggleResult('result', 'none')
     const url = `
-    http://openlibrary.org/search.json?q=${searchText}`;
-    // console.log(url)
+    https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
         .then(res => res.json())
         .then(data => searchResultDisplay(data));
 }
 const searchResultDisplay = data => {
     const books = data.docs;
-    // console.log(books);
-    // console.log(books.length);
 
-    const searchResult = document.getElementById('search-result');
-    // searchResult.textContent = '';
+    const searchResult = document.getElementById('search-display-result');
+    searchResult.textContent = '';
+
+    // counting search number 
+    const searchCountNumber = document.getElementById('result-count');
+    searchCountNumber.textContent = '';
+    const numberCountDiv = document.createElement('div');
+    searchCountNumber.appendChild(numberCountDiv);
+
+    if (books.length === 0) {
+        numberCountDiv.innerHTML = `
+        <h2 class=' text-warning text-center'>Warning!!! Please Enter Valid Input</h2> 
+        `
+    }
+    else {
+        numberCountDiv.innerHTML = `
+        <h2 class='text-center'>Searching Result is : ${data.numFound}</h2>
+        `
+    }
 
     books.forEach(book => {
-        console.log(book.tittle)
-
+        //  display searching document
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <img height="350px" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
         <div class="card-body">
-            <h2 class="card-title mb-4 fw-bold text-success">${book.title}</h2>
-            <h4 class="text-dark">Auther Name: ${book.author_name}</h4><br>
-            <h5 class="text-muted"><span class="text-dark">Publisher:</span> ${book.publisher}</h5>
+            <h3 class="card-title mb-4 fw-bold text-success">${book.title ? book.title : 'Not Found'}</h3>
+            <h5 class="text-dark mb-4">Auther Name : ${book.author_name ? book.author_name : 'Not Found'}</h5>
+            <h5 class="text-dark">Publisher : ${book.publisher ? book.publisher : 'Not Found'}</h5>
         </div>
         <div class='mx-2'>   
-        <small class="text-secondary">First Published : ${book.first_publish_year}
-        </small>
+        <h6 class="text-secondary text-center">First Published : ${book.first_publish_year ? book.first_publish_year : 'Not Found'}
+        </h6>
     </div>
     </div>
         `
         searchResult.appendChild(div)
     });
-
+    toggleResult('spinnner', 'none')
+    toggleResult('result', 'block')
 }
 
